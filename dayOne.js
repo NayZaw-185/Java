9cb03e7ad7108e673ff0e4acf3f6b077bfad65aa
nayzawnaing <nayzawnaing@nayzawnaings-MacBook-Air.local>
2025-06-11 09:32:38 +0630

    First Commit

---
+++ dayOne.js
@@ -0,0 +1,98 @@
+// let world1='Nay'
+// let world2='Zaw'
+// const fullName= world1+' '+world2;
+// console.log(fullName);
+// let example = `${world1} ${world2}`;
+// console.log(example)
+
+// 
+// let [firstName,middleName,LastName]=['Nay','Zaw','Naing'];
+// LastName='Dave';
+// console.log(firstName,middleName,LastName)
+// function addressMaker(city,state)
+// {
+//     let newaddress={city,state};//Const ဆိုရင်ထပ်သုံးလိုမရ
+//     let newaddress1={city,state};
+//     console.log(newaddress);
+// }
+// addressMaker('Mandalay','Chanayetharzan');
+// function addressMaker(address){
+//     const{city,state}=address;
+//     const newaddress={
+//         city,
+//         state,
+//         country:'Burma'
+//     }
+//     console.log(`${newaddress.city}, ${newaddress.state}, ${newaddress.country}`)
+// }
+// addressMaker({city:'Mandalay',state:'Chanayetharzan',});
+// let fullName="Nay Zaw Naing";
+// // let total=0;
+// for(const char of fullName)
+// {
+//     console.log(char);
+//     total +=income;
+ //}
+// console.log(total);
+
+// let example1=[1,2,3,4,5];
+// let example2=[...example1];
+// let example3=[...example2];
+// example3.push(true);
+// console.log(example3);
+
+// function add(nums){
+//     console.log(arguments);
+// }
+// add(4,5,6,7)
+
+// function add(...nums)
+// {
+//     console.log(nums);
+// }
+// add(4,5,6,7.8,9,10)
+
+// function add(...nums){
+//     let total=nums.reduce((x,y)=>x*y);
+//     console.log(total);
+// }
+// add(1,2,3,4,5)
+
+// function add(...nums){
+//     let total=0;
+//     nums.forEach((element)=>{
+//         total+=element;
+
+//     });
+//     console.log(total);
+
+// }
+// add(1,2,3,4,5)
+
+// let numArrays=[1,2,3,4,5];
+// console.log(numArrays.indexOf(2));
+// console.log(numArrays.includes(5));
+
+// export const data=[1,2,3];
+
+// export class Animal{
+//     constructor(type,leg)
+//     {
+//         this.type=type;
+//         this.leg=leg;
+
+//     }
+//     makeNoise(sound='loud Noise')
+//     {
+//         console.log(sound);
+//     }
+//     get metaData()
+//     {
+//         return `Type:${this.type},Legs:${this.leg}`;
+//     }
+//     static return10()
+//     {
+//         return 10;
+//     }
+// }
+
---
+++ dayTwo.js
@@ -0,0 +1,34 @@
+// import{data}from'./dayOne.js';
+// let Updateddata=data;
+// Updateddata.push(4);
+// console.log(data);
+
+// let example='Nay';
+// console.log(example.padEnd(10,'y'));
+// console.log(example.padStart(10).length
+// );
+// console.log(example.padEnd(1));
+
+// import {Animal} from'./dayOne.js';
+// let cat=new Animal('Cat',4);
+// console.log(cat.metaData);
+// cat.makeNoise('Meow');
+
+// const add=(nums) => console.log(...nums);
+// add([1,2,3,4,5]);
+
+// function multi(...nums)
+// {
+//      let total=nums.reduce((x,y)=>x*y);
+//      console.log(total);
+// }
+// multi(1,2,3,4,5);
+
+// let nums=9;
+// const add= () => (nums === '9'  ? 'True' : 'False');
+//    console.log(add()); 
+
+
+
+ 
+
